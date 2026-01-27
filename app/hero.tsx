695dc13a9427f1ba24ad78e65d0331d50c87ab9b
João Marcos Nogueira <158236298+JoaoMarcosBN1@users.noreply.github.com>
2026-01-27 15:33:53 -0400

    0.2

---
+++ app/hero.tsx
@@ -0,0 +1,36 @@
+export function Hero(){
+    return(
+        <section className="bg=[#E84C3D] text-white" overflow-hidden>
+            
+            <div>
+
+                <article>
+
+                    <div>
+                        <h1 className="text-3xl md:text-4xl font-bold leading-10">
+                            Teste
+                        </h1>
+                        <p className="lg:text-lg">
+                            teste teste teste
+                        </p>
+                        <div>
+                            <a href="#"
+                            className="bg-grenn-500 px-5 py-2 rounded-md font-smibold flex items-center justify-center w-fit"
+                            >
+                                Clique aqui
+                            </a>
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-sm mb-4"> 
+                                <b className="bg-black text-white px-2 py-1 rouded-md">Aqui</b> e um teste.
+                            </p>
+                        </div>
+                    </div>
+
+                </article>
+            
+            </div>
+
+        </section>>
+    )
+}
